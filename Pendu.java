53ff65593d5d454a348e33451575504f7da5b331
Niiunzag <43465957+Niiunzag@users.noreply.github.com>
2018-11-26 10:07:12 +0100

    Update Pendu.java

--- Pendu.java
+++ Pendu.java
@@ -1,16 +1,21 @@
 package algo.dev.project;
 
-import java.util.Scanner;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 import algo.dev.interf.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pendu extends Dico implements JeuMot{
 
-		protected List<String> motList= this.motDansTableau(this.motAleatoire());
+		protected String mot=this.motAleatoire();
+		protected List<String> motList= this.motDansTableau(this.mot);
 		protected List<Boolean> trouvee= new ArrayList<Boolean>();
 		protected List<String> affichage= new ArrayList<String>();
-		protected int nbCpRestant=5;
+		protected int nbCpRestant=10;
 		
 		public void afficherListe(List<String> l) {
 			int x=l.size();
@@ -19,7 +24,6 @@ public class Pendu extends Dico implements JeuMot{
 				System.out.print(l.get(i));
 				
 			}
-			System.out.println();
 		}
 		
 		public void initialisation() {
@@ -29,7 +33,6 @@ public class Pendu extends Dico implements JeuMot{
 				this.trouvee.add(false);
 				this.affichage.add("_ ");
 			}
-			System.out.println("Bienvenue au jeu du pendu");
 		}
 		
 		public boolean motTrouve() {
@@ -43,7 +46,7 @@ public class Pendu extends Dico implements JeuMot{
 			return test;
 		}
 		
-		public void jouerCoup(String coup){
+		public boolean jouerCoup(String coup){
 		int i;
 		int cpt=0;
 		String lettre = coup.toUpperCase();
@@ -52,7 +55,6 @@ public class Pendu extends Dico implements JeuMot{
 				this.trouvee.set(i, true);
 				this.affichage.set(i, lettre);
 				cpt++;
-				
 			}
 			
 			
@@ -60,31 +62,127 @@ public class Pendu extends Dico implements JeuMot{
 		if(cpt==0) {
 			this.nbCpRestant--;
 			System.out.println("Cette lettre n'appartient pas au mot. Nombre de vie restante: "+this.nbCpRestant);
+			return false;
 		}
+		return true;
 		}
 
 
 		public void jouer() {
 		System.out.println("( mot a trouver ) : "+this.motList);
 		this.initialisation();
-		Scanner hj=new Scanner(System.in);
+		ImageIcon img0 = new ImageIcon("pendu0.png");
+		ImageIcon img1 = new ImageIcon("pendu1.png");
+		ImageIcon img2 = new ImageIcon("pendu2.png");
+		ImageIcon img3 = new ImageIcon("pendu3.png");
+		ImageIcon img4 = new ImageIcon("pendu4.png");
+		ImageIcon img5 = new ImageIcon("pendu5.png");
+		ImageIcon img6 = new ImageIcon("pendu6.png");
+		ImageIcon img7 = new ImageIcon("pendu7.png");
+		ImageIcon img8 = new ImageIcon("pendu8.png");
+		ImageIcon img9 = new ImageIcon("pendu9.png");
+		ImageIcon victoire = new ImageIcon("victoire.gif");
 		this.afficherListe(affichage);
 		while(this.nbCpRestant>0 && this.motTrouve()==false) {
-			System.out.println("Entrer une Lettre: ");
-			String lettre = hj.next();
-			this.jouerCoup(lettre);
-			this.motTrouve();
-			this.afficherListe(affichage);
+			if(this.nbCpRestant==10) {
+				String lettre1=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre1);
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==9) {
+				String lettre2=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre2);
+				if(this.jouerCoup(lettre2)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img9);
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==8) {
+				String lettre3=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre3);
+				if(this.jouerCoup(lettre3)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img8);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==7) {
+				String lettre4=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre4);
+				if(this.jouerCoup(lettre4)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img7);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==6) {
+				String lettre5=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre5);
+				if(this.jouerCoup(lettre5)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img6);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==5) {
+				String lettre6=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre6);
+				if(this.jouerCoup(lettre6)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img5);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==4) {
+				String lettre7=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre7);
+				if(this.jouerCoup(lettre7)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img4);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==3) {
+				String lettre8=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre8);
+				if(this.jouerCoup(lettre8)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img3);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==2) {
+				String lettre9=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre9);
+				if(this.jouerCoup(lettre9)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img2);
+
+				}
+				this.motTrouve();
+			}
+			if(this.nbCpRestant==1) {
+				String lettre10=JOptionPane.showInputDialog(null,"Jouer une lettre\n"+"Nombre de coups restants: "+this.nbCpRestant+"\n"+String.join("", this.affichage),"Le jeu du pendu!", JOptionPane.QUESTION_MESSAGE);
+				this.jouerCoup(lettre10);
+				if(this.jouerCoup(lettre10)==false) {
+					JOptionPane.showMessageDialog(null, "","",JOptionPane.INFORMATION_MESSAGE,img1);
+
+				}
+				this.motTrouve();
+			}
+			
 			
 			
 		}
 		if(this.motTrouve()==true) {
-			System.out.println("VICTOIRE");
+			JOptionPane.showMessageDialog(null,"Victoire, le mot à trouver était\n"+this.mot,"Victoire", JOptionPane.INFORMATION_MESSAGE,victoire);
 		}
 		else {
-			System.out.println("DEFAITE");
+			JOptionPane.showMessageDialog(null,"Défaite, le mot à trouver était: \n"+this.mot,"Défaite", JOptionPane.INFORMATION_MESSAGE,img0);
+			
 		}
-		hj.close();
+		//hj.close();
+		
+		
 	}
 
 
