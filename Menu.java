358adefaf37663ac922907fa192c7a9b765d5251
Niiunzag <43465957+Niiunzag@users.noreply.github.com>
2018-11-30 10:24:12 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -4,15 +4,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 
-public class Menu {
+public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Dico d=new Dico();
-		Motus m=new Motus();
-		Pendu p=new Pendu();
 		Classement c=new Classement();
-		MotMele jeu = new MotMele(10);
 		String[] jeux= {"Pendu","Motus","Mots Mélés","Sudoku","Sudoku Lettres","Classement","Quitter"};
 		JOptionPane jop = new JOptionPane();
 		ImageIcon icon = new ImageIcon("becaro.gif");
@@ -20,10 +17,24 @@ public class Menu {
 		while(rang!=6) {
 			rang = jop.showOptionDialog(null, "Bienvenue je suis Thierry Becaro\n"+"Que voulez vous faire ? ", "Choix Jeu!", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, icon, jeux,jeux[0]);
 			if(rang==0) {
+				Pendu p=new Pendu();
 				p.jouer();
+				if(p.motTrouve()==true) {
+					c.nbVictPendu2++;
+				}
+				else c.nbDefPendu2++;
 			}
 			if(rang==1) {
+				Motus m=new Motus();
 				m.jouer();
+				if(m.motTrouve()==true) {
+					c.nbVictMotus2++;
+				}
+				else c.nbDefMotus2++;
+			}
+			if(rang==2) {
+				MotsMele jeu = new MotsMele();
+				jeu.jouer();
 			}
 			if(rang==5) {
 				c.afficherCLassement();
